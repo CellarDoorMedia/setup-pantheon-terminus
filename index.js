e2a68758574b7de8ebb3c1e18a799ccb6b9d2ce7
cellar-door <chris@cellardoormediagroup.com>
2021-01-28 14:49:08 -0800

    Change to tmp directory first so not to mix with the git repo

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ async function run() {
 	try {
 		const PANTHEON_MACHINE_TOKEN = core.getInput( 'pantheon-machine-token' );
 
+		await exec.exec( 'cd /tmp' );
 		await exec.exec( 'curl -O https://raw.githubusercontent.com/pantheon-systems/terminus-installer/master/builds/installer.phar' );
 		await exec.exec( 'sudo php installer.phar install' ); // Sudo is required in order to install bin/terminus.
 		await exec.exec( 'terminus', [ 'auth:login', `--machine-token=${ PANTHEON_MACHINE_TOKEN }` ] );
