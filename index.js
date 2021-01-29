95bdd3b15203cf732375e3337a3169a035369e46
cellar-door <chris@cellardoormediagroup.com>
2021-01-28 16:15:42 -0800

    updated to create the tmp directory

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ async function run() {
 	try {
 		const PANTHEON_MACHINE_TOKEN = core.getInput( 'pantheon-machine-token' );
 
+		await exec.exec( 'mkdir /tmp/terminus' );
 		await exec.exec( 'curl -o /tmp/terminus/installer.phar https://raw.githubusercontent.com/pantheon-systems/terminus-installer/master/builds/installer.phar' );
 		await exec.exec( 'sudo php /tmp/terminus/installer.phar install --install-dir=/bin' ); // Sudo is required in order to install bin/terminus.
 		await exec.exec( 'terminus', [ 'auth:login', `--machine-token=${ PANTHEON_MACHINE_TOKEN }` ] );
